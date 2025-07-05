068ae3c47e31e4acd6b90cafbf15adb774c1263f
HanzLeo <hanzincoming@icloud.com>
2025-07-05 21:17:12 +0800

    计算一段英文中的字母出现次数

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from pyproj!")
-
-
-if __name__ == "__main__":
-    main()
+sentence = input("请输入一段话: ")
+counter = {}
+for ch in sentence:
+    if "A" <= ch <= "Z" or "a" <= ch <= "z":
+        counter[ch] = counter.get(ch, 0) + 1
+sorted_keys = sorted(counter, key=counter.get, reverse=True)
+for key in sorted_keys:
+    print(f"{key} 出现了 {counter[key]} 次.")
