4de2e42811869dbb658ff4a49d29828b9938e4b0
HanzLeo <hanzincoming@icloud.com>
2025-07-06 11:06:58 +0800

    指定长度的随机验证码程序

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from math import factorial as f
+import random
+import string
 
-m = int(input("m = "))
-n = int(input("n = "))
-print(f(m) // f(n) // f(m - n))
+ALL_CHARS = string.digits + string.ascii_letters
+
+
+def generate_code(*, code_len=4):
+    return "".join(random.choices(ALL_CHARS, k=code_len))
+
+
+i = int(input("请输入想要的验证码长度："))
+n = int(input("想要几个："))
+for _ in range(1, n + 1):
+    print(generate_code(code_len=i))
