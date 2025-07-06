de92438fb6f388c4a7f7285c242f5328bc26dfc2
HanzLeo <hanzincoming@icloud.com>
2025-07-06 10:51:45 +0800

    factorial alias

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from math import factorial
+from math import factorial as f
 
 m = int(input("m = "))
 n = int(input("n = "))
-print(factorial(m) // factorial(n) // factorial(m - n))
+print(f(m) // f(n) // f(m - n))
